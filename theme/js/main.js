dc6b5709bff2403fe4bdf5a87ded830ae4529bc4
Tomaz Ursic <tomaz.ursic@gmail.com>
2019-09-17 19:56:52 +0200

    Generate Pelican site

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,28 +1,5 @@
 "use strict";
 
-// Basic stuff
-function popup() {
-  console.log(alert("Hello World Popup!"));
-}
-// setTimeout(popup, 10000);
-
-// I wish I could have used ES6 extravaganza, but not everyone supports it :(
-var r = document.getElementById("rbw"),
-  currentHue = 0,
-  hueAddition = 5,
-  documentElement = document.getElementsByTagName("html")[0],
-  clickEvent = "ontouchstart" in window ? "touchend" : "click",
-  classMethods = ["remove", "add"],
-  rainbowTiming = 1000 / 25;
-
-// function doThatColorThing() {
-//   var color = "hsl(" + currentHue + ", 80%, 60%)",
-//     nextHue = currentHue + hueAddition;
-//   currentHue = nextHue > 360 ? 0 : nextHue;
-//   r.style.color = color;
-//   setTimeout(doThatColorThing, rainbowTiming);
-// }
-
 function someControl(id, textArr, className) {
   /* You see? No fucking jQuery needed, check:
    * http://www.vanilla-js.com/
@@ -55,11 +32,5 @@ function addInvertedControl() {
   someControl("invmode", ["Night", "Day"], "inverted");
 }
 
-// // Make color shocking background
-// function psyche() {
-//   document.body.style.backgroundColor = "magenta";
-// }
-
-// doThatColorThing();
 addContrastControl();
 addInvertedControl();
